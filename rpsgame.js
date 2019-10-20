dd04f153280b92668b936d22b34ea57a25633faf
KathyPuma <kathypuma96@gmail.com>
2019-10-20 01:11:37 -0400

    adding_win_2_out_of_3

--- rpsgame.js
+++ rpsgame.js
@@ -1,42 +1,72 @@
-const readlineSync = require('readline-sync'); 
-let  rps = ["R", "P", "S"]
-
+const readlineSync = require('readline-sync');
+const  rps = ["R", "P", "S"];
 const getRandomRps = (arr) =>{
-    let random =  arr[Math.floor(Math.random()*arr.length)]
-    return random
+    let random =  arr[Math.floor(Math.random()*arr.length)];
+    return random;
 }
+
+// const answerIsRandWord = (answer,word) =>{
+//     RETURN 
+// }
+
+
+
+// console.log(answerIsRandWord("S", getRandomRps(rps)))
 
 
 const beginGame = () => {
-    let randomRps = getRandomRps(rps)
-        console.log(randomRps)
-        const answer = readlineSync.question('Pick one of [R]ock,[P]aper,[S]cissor: ') 
-        const answerUC = answer.toUpperCase()
-        // if(!rps.includes(answerUC)){
-        //     console.log("Invalid choice")
-        if( answerUC ===  randomRps) {
-            console.log("TIE")
-            beginGame()
+    
+  
+
+        
+        let userWin = 0;
+        let computerWin = 0;
+       
+        
+        while(computerWin <4 && userWin <4){
+            let randomRps = getRandomRps(rps)
+            console.log(randomRps)
+            console.log(`user wins: ${userWin}`)
+            console.log(`computer wins: ${computerWin}`)
+            const answerUC = readlineSync.question('Pick one of [R]ock,[P]aper,[S]cissor: ') 
+           
+        if(answerUC === randomRps) {
+            console.log("Its a tie")
+
+        
+           
+            
         }else if(answerUC === "R"){
             if(randomRps === "P"){
                 console.log("COMPUTER WINS")
+                computerWin++;
             }else{
                 console.log("USER WINS")
+                userWin ++;
             }
         }else if(answerUC === "P" ){
             if(randomRps === "S"){
                 console.log("COMPUTER WINS")
+                computerWin ++
+                
             }else{
                 console.log("USER WINS")
+                userWin ++
             }
         }else if(answerUC === "S"){
             if(randomRps === "R"){
                 console.log("COMPUTER WINS")
+                computerWin ++
             }else{
                 console.log("USER WINS")
+                userWin ++
             }
         }else{
             console.log("Invalid choice")
         }
+       
+    }
+    
+    
 }
 beginGame()
