634d89687ec079383596fe79e73b7d261a7b6fe3
KathyPuma <kathypuma96@gmail.com>
2019-10-20 03:36:24 -0400

    created_new_functions

--- rpsgame.js
+++ rpsgame.js
@@ -1,59 +1,69 @@
+
+
 const readlineSync = require('readline-sync');
+
 const  rps = ["R", "P", "S"];
+
+
 const getRandomRps = (arr) =>{
     let random =  arr[Math.floor(Math.random()*arr.length)];
     return random;
 }
 
+const userWinner = (answerUC, randomRps) =>{
+    if(answerUC === "R" && randomRps === "S"){
+        return " ===> User wins this round"
+    }else if(answerUC === "P" && randomRps === "R"){
+        return " ===> User wins this round"
+    }else if(answerUC === "S" && randomRps === "P"){
+        return " ===> User wins this round"
+    }
+}
 
+
+const computerWinner = (answerUC, randomRps) =>{
+    if(answerUC === "R" && randomRps === "P"){
+        return " ===> Computer wins this round"
+    }else if(answerUC === "P" && RandomRps === "S" ){
+        return " ===> Computer wins this round"
+    }else if(answerUC === "S" && randomRps === "R"){
+        return " ===> Computer wins this round"  
+    }
+}
+
+const winner = (userWin,computerWin) =>{
+    if(userWin === 3){
+        return "Congrats! You Won!"
+    }else if(computerWin === 3){
+        return "You Lose!"
+    }
+}
 
 const beginGame = () => {
     let userWin = 0;
     let computerWin = 0;
-    while(computerWin <3 && userWin <3){
+    let chances = 3;
+    while(computerWin < `${chances}` && userWin <`${chances}`){
         let randomRps = getRandomRps(rps)
-        const answer = readlineSync.question('Welcome to Rock, Paper and Scissors. The first to get 3 points, wins. Let\'s Play!\nPick one of [R]ock,[P]aper,[S]cissor: ')
+        const answer = readlineSync.question(`Welcome to Rock, Paper and Scissors. The first to get ${chances} points, wins. Let\'s Play!\nPick one of [R]ock,[P]aper,[S]cissor: `)
         let answerUC = answer.toUpperCase()
         if(answerUC === randomRps) {
             console.log("Its a tie")
-        }else if(answerUC === "R"){
-            if(randomRps === "P"){
-                console.log("Computer wins this round")
-                computerWin++;
-            }else{
-                console.log("User wins this round")
-                userWin ++;
-            }
-        }else if(answerUC === "P" ){
-            if(randomRps === "S"){
-                console.log("Computer wins this round")
-                computerWin ++    
-            }else{
-                console.log("User wins this round")
-                userWin ++
-            }
-        }else if(answerUC === "S"){
-            if(randomRps === "R"){
-                console.log("Computer wins this round")
-                computerWin ++
-            }else{
-                console.log("User wins this round")
-                userWin ++
-            }
+        }else if(computerWinner(answerUC, randomRps)){
+            console.log(computerWinner(answerUC, randomRps))
+            
+            computerWin++
+        }else if(userWinner(answerUC, randomRps)){
+            console.log(userWinner(answerUC, randomRps))
+            userWin++
         }else{
-            console.log("INVALID CHOICE")
+            console.log("===> INVALID CHOICE")
         }
         console.log(`User: ${answer} VS. Computer: ${randomRps}`)
         console.log(`User wins: ${userWin}, Computer wins: ${computerWin}`)
-        
-       
-
     } 
-    if(userWin === 3){
-        console.log("Congrats! You Won!")
-    }else if(computerWin === 3){
-        console.log("You Lose!")
-    }
+   let result = winner(userWin,computerWin)
+   console.log(result)
     
 }
 beginGame()
